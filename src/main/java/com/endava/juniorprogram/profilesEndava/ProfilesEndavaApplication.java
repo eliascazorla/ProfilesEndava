3f73d8f58292ac7e24470d6ce6dc9e45c92c2cd9
ecazorla <elias.cazorla@endava.com>
2020-11-20 17:34:02 -0300

    Proyecto creado

---
+++ src/main/java/com/endava/juniorprogram/profilesEndava/ProfilesEndavaApplication.java
@@ -0,0 +1,13 @@
+package com.endava.juniorprogram.profilesEndava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfilesEndavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfilesEndavaApplication.class, args);
+	}
+
+}
