e34368a488bf08c8cc12055b1b8cc0999c5ae854
ecazorla <elias.cazorla@endava.com>
2020-11-20 18:08:26 -0300

    funcionalidad addProfile  agregada.

--- src/main/java/com/endava/juniorprogram/profilesEndava/rest/ProfileController.java
+++ src/main/java/com/endava/juniorprogram/profilesEndava/rest/ProfileController.java
@@ -3,10 +3,7 @@ package com.endava.juniorprogram.profilesEndava.rest;
 import com.endava.juniorprogram.profilesEndava.model.Profile;
 import com.endava.juniorprogram.profilesEndava.services.IServicioProfiles;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -26,5 +23,10 @@ public class ProfileController {
     @GetMapping("{id}")
     public Optional<Profile> getProfileByDev(@PathVariable("id") long id){
         return servicioProfiles.getByDev(id);
+    }
+
+    @PostMapping
+    public Profile createProfile(@RequestBody Profile profile){
+        return servicioProfiles.addProfile(profile);
     }
 }
