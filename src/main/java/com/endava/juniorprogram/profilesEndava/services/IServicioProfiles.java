fe9f7844dff3863f96b3d20b2d48aa7929238dd7
ecazorla <elias.cazorla@endava.com>
2020-11-20 17:59:19 -0300

    funcionalidad getAll profiles agregada.

--- src/main/java/com/endava/juniorprogram/profilesEndava/services/IServicioProfiles.java
+++ src/main/java/com/endava/juniorprogram/profilesEndava/services/IServicioProfiles.java
@@ -1,4 +1,9 @@
 package com.endava.juniorprogram.profilesEndava.services;
 
+import com.endava.juniorprogram.profilesEndava.model.Profile;
+
+import java.util.List;
+
 public interface IServicioProfiles {
+    List<Profile> getAll();
 }
