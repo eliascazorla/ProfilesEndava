1a94fe98a484224ba65403f5fe8192ec37808cdf
ecazorla <elias.cazorla@endava.com>
2020-11-20 18:04:25 -0300

    funcionalidad getProfileByDev profiles agregada.

--- src/main/java/com/endava/juniorprogram/profilesEndava/rest/ProfileController.java
+++ src/main/java/com/endava/juniorprogram/profilesEndava/rest/ProfileController.java
@@ -4,10 +4,12 @@ import com.endava.juniorprogram.profilesEndava.model.Profile;
 import com.endava.juniorprogram.profilesEndava.services.IServicioProfiles;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/profiles")
@@ -19,5 +21,10 @@ public class ProfileController {
     @GetMapping
     public List<Profile> getProfiles(){
         return servicioProfiles.getAll();
+    }
+
+    @GetMapping("{id}")
+    public Optional<Profile> getProfileByDev(@PathVariable("id") long id){
+        return servicioProfiles.getByDev(id);
     }
 }
--- src/main/java/com/endava/juniorprogram/profilesEndava/services/IServicioProfiles.java
+++ src/main/java/com/endava/juniorprogram/profilesEndava/services/IServicioProfiles.java
@@ -3,7 +3,9 @@ package com.endava.juniorprogram.profilesEndava.services;
 import com.endava.juniorprogram.profilesEndava.model.Profile;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface IServicioProfiles {
     List<Profile> getAll();
+    Optional<Profile> getByDev(long id);
 }
